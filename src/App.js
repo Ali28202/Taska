250357193c0ba7288e19d2f3cf53f51b0a90e426
Ali28202 <mahallati81@gmail.com>
2024-11-06 00:14:52 +0330

    feat:navigation bar complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// import logo from "./logo.svg";
 import Navbar from "./components/Navbar";
-import "./App.css";
 
 function App() {
 	return (
-		<div className="App font-roboto">
-			<Navbar />
-		</div>
+		<>
+			<Navbar />;
+		</>
 	);
 }
 
