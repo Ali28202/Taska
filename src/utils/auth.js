c6dead06daa3470072785d61a722393a0b56296f
Ali28202 <mahallati81@gmail.com>
2024-11-28 11:54:17 +0330

    feat: add error module for sign up section

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,18 @@
 import PocketBase from "pocketbase";
-import { errorSignIn } from "./error";
+import { errorSignIn, errorSignUp } from "./error";
 const pb = new PocketBase("https://taska.liara.run");
 export async function signUp(data) {
 	try {
 		const record = await pb.collection("users").create(data);
-		const authData = await pb
-			.collection("users")
-			.authWithPassword(data.email, data.password);
-		return authData;
+		if (!record.code) {
+			const authData = await pb
+				.collection("users")
+				.authWithPassword(data.email, data.password);
+			return authData;
+		}
+		return record;
 	} catch (e) {
-		// return errorSignIn(e.data);
+		return errorSignUp(e.data);
 	}
 }
 export async function signIn(data) {
--- src/utils/error.js
+++ src/utils/error.js
@@ -14,7 +14,6 @@ export function errorPostTask(error) {
 	}
 }
 export function errorSignIn(error) {
-	console.log(error);
 	if (error.code === 400) {
 		return {
 			code: error.code,
@@ -22,4 +21,28 @@ export function errorSignIn(error) {
 			data: Object.entries(error.data),
 		};
 	}
+}
+export function errorSignUp(error) {
+	if (error.message === "Failed to create record.") {
+		if (error.code === 400) {
+			return {
+				code: error.code,
+				message: "An Error occurred",
+				data: Object.entries(error.data).filter((t) => {
+					return t[0] !== "passwordConfirm";
+				}),
+			};
+		}
+		if (error.code === 403) {
+			return {
+				code: error.code,
+				message: "You don't have permission to do this",
+			};
+		}
+	} else
+		return {
+			code: error.code,
+			data: Object.entries(error.data),
+			message: "You Should Fill The Important fields",
+		};
 }
